89df6dfde487f52b4ee2e8d632b7e2ad9ea1b98c
jasmine217k <jasmine217k@gmail.com>
2023-11-21 10:12:30 +0530

    Added Use Case-01

---
+++ src/Day2/UC1EmpAttendance.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class UC1EmpAttendance {
+
+    public static void main(String[] args)
+    {
+        int PRESENT=1;
+
+        double attendance= Math.floor(Math.random()*10) % 2;
+
+        if(attendance == PRESENT)
+            System.out.println("The employee is present");
+        else
+            System.out.println("The employee is absent");
+
+    }
+}
